f7349266dcd06bad0829e455ce786b91e724cfb1
Nirmal Sunny <nirmalsunny24@gmail.com>
2023-12-18 13:03:35 +0000

    create a server on port 8080

--- main.go
+++ main.go
@@ -2,10 +2,18 @@ package main
 
 import (
 	"fmt"
-
-	"golang.org/x/example/stringutil"
+	"net/http"
 )
 
 func main() {
-	fmt.Println(stringutil.Reverse("!selpmaxe oG ,olleH"))
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Hello, World! This is your Go server.")
+	})
+
+	port := 8080
+	fmt.Printf("Server is running on port %d...\n", port)
+	err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+	if err != nil {
+		fmt.Println("Error starting the server:", err)
+	}
 }
